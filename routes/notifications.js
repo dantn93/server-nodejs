f365a7e1721549259bb2d60504a572a8c50e5da2
Dan <dantn93@gmail.com>
2022-06-06 16:27:31 +0700

    format

--- routes/notifications.js
+++ routes/notifications.js
@@ -7,44 +7,44 @@ const admin = require('../helper');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
-	// Push notificaton => Todo
-	// 1. Push notificaton to device
-	sendToDevice();
-	// 2.
+  // Push notificaton => Todo
+  // 1. Push notificaton to device
+  sendToDevice();
+  // 2.
 
-	res.status(200).send({
-		notification: 'This notification'
-	})
+  res.status(200).send({
+    notification: 'This notification'
+  })
 });
 
 const sendToDevice = () => {
-	// This registration token comes from the client FCM SDKs. (device Token)
-	const registrationToken = 'c9-n8e4xM0rysPz2Tedv3q:APA91bFNuZcuPr9cpFLR2HzkEEn5jeGp6RFbs-5wUAhRc47u0OIsh0AebUbamCwAIZFUw3Ix1yJZZdgqcY0zMdvlRUknVMWVYoSgzB5V3_WmVI10bwNtCYENOA97m6BB33J3vyB7r7-j';
+  // This registration token comes from the client FCM SDKs. (device Token)
+  const registrationToken = 'dAcMBfXbRr6shRvKNcN7Fs:APA91bHlC15EeX9ajeQcrGbcBG8l4o0Qdl06YpdGe_2FeCQLMmqs0KodVbFr3wneoBQ0RTlwHxquif7CUy0Kp3IAU7fPLKC6r9bitfF7ObkqT4qAqNnJN4m4GyjltZyjlSMvgWUCJmMM';
 
-	// Send a message to the device corresponding to the provided
-	// registration token.
-	admin.messaging().send({
-		notification: {
-			"title": 'This is a title',
-			"body": 'This is a body',
-		},
-		"android": {
-			"notification": {
-				"sound": "default"
-			}
-		},
-		"apns": {
-			"payload": {
-				"aps": {
-					"sound": "default"
-				}
-			}
-		},
-		token: registrationToken,
-	})
-		.then(res => console.log(JSON.stringify(res)))
-		.catch(err => console.log(JSON.stringify(err)))
-	return '';
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  admin.messaging().send({
+    notification: {
+      "title": 'This is a title',
+      "body": 'This is a body',
+    },
+    "android": {
+      "notification": {
+        "sound": "default"
+      }
+    },
+    "apns": {
+      "payload": {
+        "aps": {
+          "sound": "default"
+        }
+      }
+    },
+    token: registrationToken,
+  })
+    .then(res => console.log(JSON.stringify(res)))
+    .catch(err => console.log(JSON.stringify(err)))
+  return '';
 }
 
 module.exports = router;
